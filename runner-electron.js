651780daafe1c73370a3f4a9d5594d862ad1d4a5
mackz <90354303+adsmackz@users.noreply.github.com>
2024-04-17 23:34:35 -0300

    Update runner-electron.js

--- runner-electron.js
+++ runner-electron.js
@@ -1,14 +1,12 @@
 const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
-const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImQ0Mzg2Nzk0LTAxMjEtNDhkMS04ZjUwLWMwYjhmMjdhOGI1ZC0xNzEzNDA1OTMzNDQzIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiNTk3MDk2OGUtODdjYi00YWQ1LWI1ZWQtYWU3Njc5NWYwYzJmIiwidHlwZSI6InQifQ.2YcnxYFua-UqITZAsHKl4SEO6wc1VjPdkFwjCgJZJFw'
-
 cypress.run({
   // specs to run here
 })
 .then((results) => {
   const args = {
-    target: TOKEN,
+    target: process.env.TARGET_TOKEN_ELECTRON,
   }
   tesults.results(results, args);
 })
